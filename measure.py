31915cbbb6715c8d718bbb3586739ec1034251f6
Christian Benke <cbenke@anexia-it.com>
2023-10-06 16:57:01 +0200

    Full mqtt reading and publishing for temp sensors

--- measure.py
+++ measure.py
@@ -1,20 +1,18 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
 
-import re, os, time
-
-import json                                                                                                                                                                                                        
+import json
 import logging
+import re
 import time
 
-import requests
 import paho.mqtt.client as mqtt
 
 # MQTT configuration
 MQTT_BROKER_IP = "10.0.0.30"
 MQTT_BASE_TOPIC = "homeassistant/sensor/heizung/"
-MQTT_BASE_ID = "heizung_temperature"
-MQTT_NAME_SUFFIX = "heizung"
+MQTT_BASE_ID = "heizung_temperatur_"
+MQTT_NAME_PREFIX = "Heizung"
 MQTT_RETAIN = True  # MQTT broker keeps the last message
 MQTT_QOS = 2  # Make sure the MQTT message is only sent once
 
@@ -23,18 +21,23 @@ MQTT_QOS = 2  # Make sure the MQTT message is only sent once
 LOGLEVEL = logging.DEBUG
 logging.basicConfig(level=LOGLEVEL)
 
-SENSOR_PARAMS = [
+SENSORS = [
     {
-        "name": "Heizung Rücklauf",
-        "hass_key": "heizung_ruecklauf",
-        "path":"/sys/bus/w1/devices/28-c6ad451f64ff/w1_slave",
-    },
+        "name": "Rücklauf Gesamt",
+        "key": "ruecklauf_gesamt",
+        "path": "/sys/bus/w1/devices/28-c6ad451f64ff/w1_slave",
+    }
 ]
+
+SENSOR_PARAMS = {
+    "device_class": "temperature",
+    "unit": "°C"
+}
 
 
 # function: read and parse sensor data file
 def read_sensor(path):
-    measurement = "U"
+    measurement = "0"
     try:
         f = open(path, "r")
         line = f.readline()
@@ -44,56 +47,51 @@ def read_sensor(path):
             if m:
                 measurement = str(float(m.group(2)) / 1000.0)
             f.close()
-    except IOError as e: 
+    except IOError as e:
         print(time.strftime("%x %X"), "Error reading", path, ": ", e)
     return measurement
 
 
-def get_sensor_data():
+def get_sensor_temps():
     # read values from all sensors
-    sensor_data = [] 
-    for sensor in SENSOR_PARAMS:
-        sensor['temperature_value'] = read_sensor(sensor['path'])
-        sensor['device_class'] = 'temperature'
-        sensor_data.append(sensor)
-    return sensor_data
+    sensor_temps = []
+    for sensor in SENSORS:
+        sensor['temperature'] = read_sensor(sensor['path'])
+        sensor_temps.append(sensor)
+    return sensor_temps
 
 
 def mqtt_publish_config(mqtt_client):
-    for param in STATION_PARAMS.keys():
-        if param in IGNORE_KEYS:
-            continue
-
-        name = STATION_PARAMS[param]["name"]  # Entity name listed in HASS (Home Assistant) (e.g. "Temperature")
-        device_class = STATION_PARAMS[param]["device_class"]  # defines the icon used in HASS (e.g. "temperature")
-        unit = STATION_PARAMS[param]["unit"]  # defines the unit used in HASS (e.g. "°C")
-        unique_id = f"{MQTT_BASE_ID}{param}"  # unique identifier used in HASS (e.g. "tawes_temperature")
+    for sensor in SENSORS:
+        name = sensor["name"]  # Entity name listed in HASS (Home Assistant) (e.g. "Temperature")
+        device_class = SENSOR_PARAMS["device_class"]  # defines the icon used in HASS (e.g. "temperature")
+        unit = SENSOR_PARAMS["unit"]  # defines the unit used in HASS (e.g. "°C")
+        unique_id = f"{MQTT_BASE_ID}{sensor['key']}"  # unique identifier used in HASS (e.g. "tawes_temperature")
         value_template = f"{{{{ value_json.{unique_id}}}}}"  # defines how HASS extracts the value of this entity
         topic = f"{MQTT_BASE_TOPIC}{unique_id}/config"
 
         payload = {
             "device_class": device_class,
-            "name": f"{name} {MQTT_NAME_SUFFIX}",
+            "name": f"{MQTT_NAME_PREFIX} {name}",
             "state_topic": f"{MQTT_BASE_TOPIC}state",
             "unit_of_measurement": unit,
             "value_template": value_template,
             "unique_id": unique_id
         }
 
+        logging.debug(f"config topic: {topic}")
+        logging.debug(f"config payload: {payload}")
         mqtt_client.publish(topic, payload=json.dumps(payload), qos=MQTT_QOS, retain=MQTT_RETAIN)
 
 
-def mqtt_publish_state(mqtt_client, weather_data):
+def mqtt_publish_state(mqtt_client, sensor_data):
     topic = f"{MQTT_BASE_TOPIC}state"
     payload = dict()
-    for key, value in weather_data.items():
-        try:
-            value = int(value)
-        except ValueError:
-            value = float(value.replace(",", "."))
+    for sensor in sensor_data:
+        payload[f"{MQTT_BASE_ID}{sensor['key']}"] = sensor["temperature"]
 
-        payload[f"{MQTT_BASE_ID}{key}"] = value
-
+    logging.debug(f"state topic: {topic}")
+    logging.debug(f"state payload: {payload}")
     mqtt_client.publish(topic, payload=json.dumps(payload), qos=MQTT_QOS, retain=MQTT_RETAIN)
 
 
@@ -105,8 +103,8 @@ def on_connect(client, userdata, flags, rc):
         logging.debug("Bad connection Returned code=", rc)
 
 
-def mqtt_run(station_weather):
-    mqtt_client = mqtt.Client("tawes")
+def mqtt_run(sensor_temperatures):
+    mqtt_client = mqtt.Client("DS1820")
     mqtt.Client.connected_flag = False
     logging.debug(f"Connecting to mqtt_broker_ip {MQTT_BROKER_IP}")
 
@@ -121,13 +119,13 @@ def mqtt_run(station_weather):
     logging.debug("Connected to mqtt broker")
 
     mqtt_publish_config(mqtt_client)
-    mqtt_publish_state(mqtt_client, sensor_data)
+    mqtt_publish_state(mqtt_client, sensor_temperatures)
 
     mqtt_client.loop_stop()
-    mqtt_client.disconnect(
+    mqtt_client.disconnect()
+
 
 if __name__ == "__main__":
-    sensor_data = get_sensor_data()
-    logging.debug(sensor_data)
-    mqtt_run(sensor_data)
-
+    sensor_temperatures = get_sensor_temps()
+    logging.debug(sensor_temperatures)
+    mqtt_run(sensor_temperatures)
